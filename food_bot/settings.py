a70d8be92553f679a40813594493df87a865852b
root <root@ovz1.j80844486.pl50n.vps.myjino.ru>
2022-10-05 17:31:35 +0000

    Fixed https login

--- food_bot/settings.py
+++ food_bot/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["127.0.0.1", "pyprojects.ru"]
 
-CSRF_TRUSTED_ORIGINS = ['https://pyprojects.ru/']
+CSRF_TRUSTED_ORIGINS = ['https://pyprojects.ru']
 
 
 # Application definition
