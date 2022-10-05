520c8eae6a484ec211e7401a2740c57f481cc2c9
MrSabin <sabin.21011986@gmail.com>
2022-10-05 19:14:56 +0300

    Edited models

--- bot_db/admin.py
+++ bot_db/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Diet, Meal, Ingredient
+from .models import Diet, Meal, Ingredient, Product_group
 
 admin.site.register(Diet)
 admin.site.register(Meal)
 admin.site.register(Ingredient)
+admin.site.register(Product_group)
--- bot_db/models.py
+++ bot_db/models.py
@@ -8,8 +8,18 @@ class Diet(models.Model):
         return self.title
 
 
+class Product_group(models.Model):
+    title = models.CharField(max_length=255, verbose_name="Группа продуктов")
+
+    def __str__(self):
+        return self.title
+
+
 class Ingredient(models.Model):
     title = models.CharField(max_length=255, verbose_name="Название продукта")
+    group = models.ForeignKey(
+        Product_group, on_delete=models.CASCADE, verbose_name="Продуктовая группа"
+    )
 
     def __str__(self):
         return self.title
@@ -17,7 +27,10 @@ class Ingredient(models.Model):
 
 class Meal(models.Model):
     title = models.CharField(max_length=255, verbose_name="Название блюда")
-    diet = models.ForeignKey(Diet, on_delete=models.CASCADE)
+    diet = models.ForeignKey(
+        Diet, on_delete=models.CASCADE, verbose_name="Тип диеты")
+    ingredients = models.ManyToManyField(
+        Ingredient, verbose_name="Ингредиенты")
 
     def __str__(self):
         return self.title
