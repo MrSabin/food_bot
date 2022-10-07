3cf5d2a73c68dc7047d2c2977140c4290e4c730f
MrSabin <sabin.21011986@gmail.com>
2022-10-07 22:13:26 +0300

    Edited models

--- bot_db/models.py
+++ bot_db/models.py
@@ -8,7 +8,7 @@ class Diet(models.Model):
         return self.title
 
 
-class Product_group(models.Model):
+class ProductGroup(models.Model):
     title = models.CharField(max_length=255, verbose_name="Группа продуктов")
 
     def __str__(self):
@@ -18,19 +18,17 @@ class Product_group(models.Model):
 class Ingredient(models.Model):
     title = models.CharField(max_length=255, verbose_name="Название продукта")
     group = models.ForeignKey(
-        Product_group, on_delete=models.CASCADE, verbose_name="Продуктовая группа"
+        ProductGroup, on_delete=models.CASCADE, verbose_name="Продуктовая группа"
     )
 
     def __str__(self):
         return self.title
 
 
-class Meal(models.Model):
+class Recipe(models.Model):
     title = models.CharField(max_length=255, verbose_name="Название блюда")
-    diet = models.ForeignKey(
-        Diet, on_delete=models.CASCADE, verbose_name="Тип диеты")
-    ingredients = models.ManyToManyField(
-        Ingredient, verbose_name="Ингредиенты")
+    diet = models.ForeignKey(Diet, on_delete=models.CASCADE, verbose_name="Тип диеты")
+    ingredients = models.ManyToManyField(Ingredient, verbose_name="Ингредиенты")
 
     def __str__(self):
         return self.title
@@ -39,12 +37,11 @@ class Meal(models.Model):
 class User(models.Model):
     user_id = models.IntegerField(verbose_name="Telegram ID")
     full_name = models.CharField(max_length=50, verbose_name="Полное имя")
-    phone_number = models.CharField(
-        max_length=30, verbose_name="Номер телефона")
+    phone_number = models.CharField(max_length=30, verbose_name="Номер телефона")
 
     def __str__(self):
         return self.full_name
-    
+
 
 class Subscription(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
