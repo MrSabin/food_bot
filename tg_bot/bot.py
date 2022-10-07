af0403236ee31d067a545cf335d7ec630ed2ad1c
mavel-x <96616212+mavel-x@users.noreply.github.com>
2022-10-08 00:22:45 +0300

    add bot logic of new recipe branch

--- tg_bot/bot.py
+++ tg_bot/bot.py
@@ -1,10 +1,10 @@
 import logging
-
+from pathlib import Path
 
 from django.views.decorators.csrf import csrf_exempt
 from django.core.management.base import BaseCommand
 from environs import Env
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update, InputMediaPhoto
 from telegram import error as telegram_error
 from telegram.ext import (
     CallbackContext,
@@ -108,12 +108,10 @@ def main_menu(update: Update, context: CallbackContext):
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
 
-    if query:
-        query.edit_message_text(message_text, reply_markup=reply_markup)
-    else:
-        update.effective_chat.send_message(message_text, reply_markup=reply_markup)
+    update.effective_chat.send_message(message_text, reply_markup=reply_markup)
 
-    return CHOOSE_FROM_MAIN_MENU
+    if query:
+        query.message.delete()
 
 
 def request_diet(update: Update, context: CallbackContext):
@@ -129,34 +127,113 @@ def request_diet(update: Update, context: CallbackContext):
     keyboard.append([InlineKeyboardButton(bot_strings.back_button, callback_data='back_to_main')])
 
     reply_markup = InlineKeyboardMarkup(keyboard)
-    query.edit_message_text(message_text, reply_markup=reply_markup)
-
-    return AWAIT_DIET
+    update.effective_chat.send_message(message_text, reply_markup=reply_markup)
+    if query:
+        query.message.delete()
 
 
 def show_new_recipe(update: Update, context: CallbackContext):
     # TODO check excluded recipes
     # TODO no recipes with allergy ingredients
+
     query = update.callback_query
     query.answer()
 
     selected_diet = query.data
-    # TODO get a recipe from Django
 
-    message_text = f'option selected: {query.data} \n CONTENT: RECIPE WITH PHOTO \n CONVERSATION STAGE: SHOW_NEW_RECIPE'
-    query.edit_message_text(message_text)
+    # TODO get a recipe from Django
+    recipe_id = 123
+    recipe_title = 'Replace'
+    recipe_description = 'Me'
+    recipe_ingredients = ('With: data\n'
+                          'from: Django')
+    recipe_photo = 'https://ic.pics.livejournal.com/maxim_nm/51556845/6518023/6518023_original.jpg'
+
+    message_text = bot_strings.recipe.format(title=recipe_title,
+                                             description=recipe_description,
+                                             ingredients=recipe_ingredients)
+
+    keyboard = [
+        [
+            InlineKeyboardButton(bot_strings.add_to_favorites_button,
+                                 callback_data=f'add_to_favorites_{recipe_id}'),
+        ],
+        [
+            InlineKeyboardButton(bot_strings.exclude_recipe_button,
+                                 callback_data=f'exclude_recipe_{recipe_id}'),
+        ],
+        [
+            InlineKeyboardButton(bot_strings.another_recipe_same_diet, callback_data=selected_diet),
+        ],
+        [
+            InlineKeyboardButton(bot_strings.another_recipe_diff_diet, callback_data='new_recipe'),
+        ],
+        [
+            InlineKeyboardButton(bot_strings.back_button, callback_data='back_to_main'),
+        ],
+
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    update.effective_chat.send_photo(recipe_photo, caption=message_text, reply_markup=reply_markup)
+    if query:
+        query.message.delete()
 
 
 def add_recipe_to_favorites(update: Update, context: CallbackContext):
-    pass
+    query = update.callback_query
+    query.answer()
+
+    recipe_id = int(query.data.removeprefix('add_to_favorites_'))
+
+    # TODO Django add Recipe to User's favorites
+    print(f'added recipe {recipe_id} to favorites.')
+
+    message_text = bot_strings.recipe_added_to_favorites
+
+    keyboard = [[
+        InlineKeyboardButton(bot_strings.cancel_button, callback_data=f'cancel_favorite_{recipe_id}'),
+    ]]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    update.effective_chat.send_message(message_text, reply_markup=reply_markup)
 
 
 def exclude_recipe(update: Update, context: CallbackContext):
-    pass
+    query = update.callback_query
+    query.answer()
+
+    recipe_id = int(query.data.removeprefix('exclude_recipe_'))
+
+    # TODO Django add Recipe to User's excluded
+    print(f'excluded recipe {recipe_id}.')
+
+    message_text = bot_strings.recipe_excluded
+
+    keyboard = [[
+        InlineKeyboardButton(bot_strings.cancel_button, callback_data=f'cancel_exclude_{recipe_id}'),
+    ]]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    update.effective_chat.send_message(message_text, reply_markup=reply_markup)
 
 
 def cancel_preference_operation(update: Update, context: CallbackContext):
-    pass
+    query = update.callback_query
+    query.answer()
+
+    _, operation_type, recipe_id = query.data.split('_')
+
+    if operation_type == 'favorite':
+        # TODO remove recipe_id from User.favorite_recipes
+        pass
+
+    else:
+        # TODO remove recipe_id from User.excluded_recipes
+        pass
+
+    message_text = bot_strings.operation_cancelled
+    query.edit_message_text(message_text)
 
 
 def account_menu(update: Update, context: CallbackContext):
@@ -180,7 +257,6 @@ def account_menu(update: Update, context: CallbackContext):
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
     query.edit_message_text(message_text, reply_markup=reply_markup)
-    return CHOOSE_FROM_ACCOUNT_MENU
 
 
 def favorite_recipes(update: Update, context: CallbackContext):
@@ -237,11 +313,6 @@ def add_allergy_to_ingredient_group(update: Update, context: CallbackContext):
     pass
 
 
-def back_to_main(update: Update, context: CallbackContext):
-    """A command that can be selected at any time to abandon an incomplete branch and go to main menu"""
-    pass
-
-
 def help_message(update: Update, context: CallbackContext):
     """Send help text"""
     update.effective_chat.send_message('TEXT: HELP')
@@ -266,8 +337,6 @@ class Command(BaseCommand):
         conversation_handler = ConversationHandler(
             entry_points=[
                 CommandHandler('start', start),
-                CallbackQueryHandler(..., pattern=r'^...$'),
-                MessageHandler(Filters.text, help_message),
             ],
             states={
                 AWAIT_REGISTRATION: [CallbackQueryHandler(request_name, pattern=r'^register$')],
@@ -276,34 +345,21 @@ class Command(BaseCommand):
                     CallbackQueryHandler(request_name, pattern=r'^back_to_name$'),
                     CallbackQueryHandler(complete_registration, pattern=r'^confirm$')
                 ],
-                CHOOSE_FROM_MAIN_MENU: [
-                    CallbackQueryHandler(request_diet, pattern=r'^new_recipe$'),
-                    CallbackQueryHandler(account_menu, pattern=r'^account$'),
-                ],
-                AWAIT_DIET: [
-                    CallbackQueryHandler(show_new_recipe, pattern=r'^diet_\d+$|^diet_any$'),
-                    CallbackQueryHandler(main_menu, pattern=r'^back_to_main$'),
-                ],
-                SHOW_NEW_RECIPE: [
-                    CallbackQueryHandler(add_recipe_to_favorites, pattern=r'^...$'),
-                    CallbackQueryHandler(exclude_recipe, pattern=r'^...$')
-                ],
-                CHOOSE_FROM_ACCOUNT_MENU: [
-                    CallbackQueryHandler(favorite_recipes, pattern=r'^favorite_recipes$'),
-                    CallbackQueryHandler(excluded_recipes, pattern=r'^excluded_recipes$'),
-                    CallbackQueryHandler(allergies, pattern=r'^allergies$'),
-                    CallbackQueryHandler(main_menu, pattern=r'^back_to_main$'),
-                ]
-
-                # ...: ...,
             },
             fallbacks=[
-                CallbackQueryHandler(back_to_main, pattern=r'^back_to_main$'),
-                CommandHandler('back_to_main', back_to_main),
-                CommandHandler('start', main_menu),
+                CommandHandler('start', start),
                 MessageHandler(Filters.text, help_message),
                 ]
         )
+
+        dispatcher.add_handler(CallbackQueryHandler(account_menu, pattern=r'^account$'))
+        dispatcher.add_handler(CallbackQueryHandler(request_diet, pattern=r'^new_recipe$'))
+        dispatcher.add_handler(CallbackQueryHandler(show_new_recipe, pattern=r'^diet_\d+$|^diet_any$'))
+        dispatcher.add_handler(CallbackQueryHandler(add_recipe_to_favorites, pattern=r'^add_to_favorites_\d+$'))
+        dispatcher.add_handler(CallbackQueryHandler(exclude_recipe, pattern=r'^exclude_recipe_\d+$'))
+        dispatcher.add_handler(CallbackQueryHandler(cancel_preference_operation, pattern=r'^cancel_.+$'))
+
+        dispatcher.add_handler(CallbackQueryHandler(main_menu, pattern=r'^back_to_main$'))
 
         dispatcher.add_handler(conversation_handler)
 
