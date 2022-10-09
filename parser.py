b9eec2c581de3ec87db26bb801dd9b5310337bba
MrSabin <sabin.21011986@gmail.com>
2022-10-09 22:32:08 +0300

    Simplified parser

--- parser.py
+++ parser.py
@@ -43,24 +43,11 @@ def get_recipe_details(urls_list, diet):
 
         image_url = soup.find("img", class_="resultphoto").get("src")
 
-        ingredients = {}
+        ingredients = []
         ingredients_soup = soup.find_all("p", itemprop="recipeIngredient")
 
         for ingredient_entity in ingredients_soup:
-            if " – " in ingredient_entity.text:
-                ingredient_details = ingredient_entity.text.split(" – ")
-            elif " - " in ingredient_entity.text:
-                ingredient_details = ingredient_entity.text.split(" - ")
-            else:
-                ingredient_details = ingredient_entity.text.split(" — ")
-
-            ingredient = ingredient_details[0]
-            try:
-                amount = ingredient_details[1]
-            except IndexError:
-                amount = ""
-
-            ingredients[ingredient] = amount
+            ingredients.append(ingredient_entity.text)
 
         recipe = {}
         recipe["title"] = title
@@ -79,10 +66,14 @@ def main():
     non_gluten_url = "https://www.iamcook.ru/event/baking/gluten-free-baking"
     abstinence_url = "https://www.iamcook.ru/event/abstinence"
     everyday_url = "https://www.iamcook.ru/event/everyday"
-    get_recipe_details(get_recipes_links(vegetarian_url, pages), diet="Вегетарианская")
-    get_recipe_details(get_recipes_links(non_gluten_url, pages), diet="Безглютеновая")
-    get_recipe_details(get_recipes_links(abstinence_url, pages), diet="Постная")
-    get_recipe_details(get_recipes_links(everyday_url, pages), diet="На каждый день")
+    get_recipe_details(get_recipes_links(
+        vegetarian_url, pages), diet="Вегетарианская")
+    get_recipe_details(get_recipes_links(
+        non_gluten_url, pages), diet="Безглютеновая")
+    get_recipe_details(get_recipes_links(
+        abstinence_url, pages), diet="Постная")
+    get_recipe_details(get_recipes_links(
+        everyday_url, pages), diet="На каждый день")
 
 
 if __name__ == "__main__":
